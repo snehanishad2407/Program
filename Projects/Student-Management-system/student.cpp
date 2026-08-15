e949eca4b0e8a158c05f2cd6d70dba5dff26edfa
snehanishad2407 <neetunishad2045@gmail.com>
2026-08-15 21:37:03 +0530

    Add student input and menu

--- Projects/Student-Management-system/student.cpp
+++ Projects/Student-Management-system/student.cpp
@@ -1,1 +1,27 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+  int choice;
+  cout<<"==== Student Management System ===="<<endl;
+  cout<<"1. Add Student"<<endl;
+  cout<<"2. Display Student"<<endl;
+  cout<<"3. Exit"<<endl;
+  cout<<"Enter your choice:";
+  cin>>choice;
+  string name;
+  int age;
+  int rollNo;
+  cout<<"Enter student name:";
+  getline(cin,name);
+  cout<<"Enter age:";
+  cin>>age;
+  cout<<"Enter Roll number:";
+  cin>>rollNo;
+  cout<<"\n--- Student Details ---"<<endl;
+  cout<<"Name:"<<name<<endl;
+    cout<<"Age:"<<age<<endl;
+  cout<<"Roll Number:"<<rollNo<<endl;
+  
+  return 0;
+}
