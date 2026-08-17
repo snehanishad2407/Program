6623189c0317ed554943787605c0b9e10c950e2d
snehanishad2407 <neetunishad2045@gmail.com>
2026-08-17 19:38:51 +0530

    Grade using if-else statement

--- C++/ifelse.cpp
+++ C++/ifelse.cpp
@@ -5,20 +5,24 @@ int main(){
     
     cout<<"Enter your marks=";
     cin>>marks;
+    //Check for invalid input
+    if(marks<0|| marks>100){
+        cout<<"Invalid Marks! Please enter a value between 0 and 100.\n";
+    }
     if(marks>=90){
-        cout<<"Grade A";
+        cout<<"Grade A\n";
     }
     else if(marks>=75){
-        cout<<"Grade B";
+        cout<<"Grade B\n";
 
     }else if(marks>=60){
-        cout<<"Grade C";
+        cout<<"Grade C\n";
 
     }else if(marks>=50){
-        cout<<"Grade D";
+        cout<<"Grade D\n";
 
     }else{
-        cout<<"Grade F";
+        cout<<"Grade E\n";
     }
       return 0;
     }
