4679d3527e0f046ac08926de3e3f1dbbfe2e9c87
snehanishad2407 <neetunishad2045@gmail.com>
2026-08-22 19:52:29 +0530

    Relational operators

---
+++ Python/relational.py
@@ -0,0 +1,9 @@
+a=50
+b=20
+
+print("Equality:",a==b)
+print("Inequality:",a!=b)
+print("Greater than:",a>b)
+print("Greater than or equal to:",a>=b)
+print("Less than:",a<b)
+print("Less than or equal to:",a<=b)
