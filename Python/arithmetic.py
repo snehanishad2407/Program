744f760e2bb5a4ab7a380cc797b88a38c8212f67
snehanishad2407 <neetunishad2045@gmail.com>
2026-08-22 10:40:59 +0530

    Arithmetic operators

---
+++ Python/arithmetic.py
@@ -0,0 +1,10 @@
+#arithmetic operators
+
+a=10
+b=2
+print("Addition=",a+b)
+print("Subtraction=",a-b)
+print("Multiplication=",a*b)
+print("Division=",a/b)
+print("Modulus=",a%b)
+print("Exponentiation=",a**b)
