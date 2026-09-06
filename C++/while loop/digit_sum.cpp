e906ee5508ee4f051bcb4d420df3483cd54f1170
snehanishad2407 <neetunishad2045@gmail.com>
2026-09-06 20:41:14 +0530

    Sum of the digits code

---
+++ C++/while loop/digit_sum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum=0;
+    cout<<"Enter any number to find sum of digits=";
+    cin>>n;
+        while(n>0)
+    {
+        sum=sum+n%10;
+        n=n/10;
+
+    }
+    cout<<"Sum of digits is="<<sum<<endl;
+
+return 0;
+}
