4db7fb02ad91cd0a26ba217a2c58f9f9b2d0709b
snehanishad2407 <neetunishad2045@gmail.com>
2026-09-10 10:47:48 +0530

    Reverse number printing  (N numbers}

---
+++ C++/for loop/reverse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"Enter any no. for which you want to find reverse of that number=";
+    cin>>n;
+    cout<<"Reverse number from "<<n<<" to 1 is="<<endl;
+    for(int i=n;i>=1;i--)
+    {
+        cout<<i<<endl;
+    
+    }
+return 0;
+}
