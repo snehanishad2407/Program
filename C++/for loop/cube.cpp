0d55cf0fc1ac9e14a2e5164a7a45487001ce14f4
snehanishad2407 <neetunishad2045@gmail.com>
2026-09-12 11:34:52 +0530

    Cube numbers printing from 1 to N

--- C++/for loop/cube.cpp
+++ C++/for loop/cube.cpp
@@ -5,7 +5,7 @@ int main()
     int i,n;
     cout<<"Enter any number upto which you can find cubes=";
     cin>>n;
-    cout<<"Cube numbers from 1 to "<<n<< "are="<<endl;
+    cout<<"Cube numbers from 1 to "<<n<< " are="<<endl;
     for(int i=1;i<=n;i++)
     {
         cout<<"Cube of "<<i<<" is = "<<i*i*i<<endl; 
