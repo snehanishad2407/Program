32b6aa5371b9e2b205197f6119e9d9c1f7974c79
snehanishad2407 <neetunishad2045@gmail.com>
2026-09-12 11:22:57 +0530

    Cube of number from 1 to N

---
+++ C++/for loop/cube.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"Enter any number upto which you can find cubes=";
+    cin>>n;
+    cout<<"Cube numbers from 1 to "<<n<< "are="<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<"Cube of "<<i<<" is = "<<i*i*i<<endl; 
+}
+    return 0;
+}
